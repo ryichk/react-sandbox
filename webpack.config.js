83b1996c34a0671c921dcd1509bcd28914bfee0c
ryichk <ryichk@gmail.com>
2021-03-22 02:43:45 +0900

    change entry file app.js to index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public')
